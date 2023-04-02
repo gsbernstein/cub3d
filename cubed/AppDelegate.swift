27b2d00d8fdc960fd6c4fcdbd597e15654d10c27
Greg Bernstein <greg@vida.com>
2023-04-02 11:43:22 -0700

    Initial Commit

---
+++ cubed/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  cubed
+//
+//  Created by Greg Bernstein on 4/2/23.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
